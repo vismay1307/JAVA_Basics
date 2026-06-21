08b23e9a0e43ffe4e850bca56eeb66fb405d23e8
vismaythakkar-admin <sepl.vismaythakkar@gmail.com>
2026-06-21 10:04:33 +0530

    comment

--- reversestring.java
+++ reversestring.java
@@ -18,3 +18,33 @@ public class reversestring {
         sc.close();
     }
 }
+
+
+
+// import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String[] args) {
+
+//         Scanner sc = new Scanner(System.in);
+
+//         System.out.println("Enter String:");
+//         String str = sc.nextLine();
+
+//         int end = str.length();
+
+//         for(int i = str.length() - 1; i >= 0; i--) {
+
+//             if(str.charAt(i) == ' ') {
+
+//                 System.out.print(str.substring(i + 1, end) + " ");
+
+//                 end = i;
+//             }
+//         }
+
+//         System.out.print(str.substring(0, end));
+
+//         sc.close();
+//     }
+// }
